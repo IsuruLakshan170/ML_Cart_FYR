f0723771341e6b002b85715dbdf99ee2fae2dd54
IsuruLakshan170 <82941889+IsuruLakshan170@users.noreply.github.com>
2023-03-02 20:01:04 +0530

    add tflite model save function

--- saveModelData.py
+++ saveModelData.py
@@ -1,13 +1,15 @@
 
 import os
 import tensorflow as tf
+from tensorflow import keras
 
 def saveModelData(model):
     #save model
     model.save('dataset/model.h5')
     #save model parameters
     model.save_weights('dataset/model_weights.h5')
-
+    #convert and save model into tensorflow type
+    # convertToTenserflowModel(model)
     # Get the size of the saved model file
     model_size_bytes = os.path.getsize('dataset/model.h5')
     # Convert bytes to MB
@@ -22,3 +24,12 @@ def saveModelData(model):
     model_size_mb = model_size_bytes / (1024 * 1024)
 
     print(f"The size of the saved model parameters file is {model_size_mb:.2f} MB.")
+    
+    
+def convertToTenserflowModel(model):
+    # Convert your Keras model to a TensorFlow Lite model
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    tflite_model = converter.convert()
+
+    # Save the TensorFlow Lite model to disk
+    open("modelData/model.tflite","wb").write(tflite_model)
