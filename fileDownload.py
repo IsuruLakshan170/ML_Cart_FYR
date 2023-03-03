d1f9e873219f1ed406c35aa729a8326555936f5b
IsuruLakshan170 <82941889+IsuruLakshan170@users.noreply.github.com>
2023-03-03 16:50:01 +0530

    add image download

---
+++ fileDownload.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def downloadFile(url,filename):
+    try:
+        with requests.get(url) as req:
+            with open(filename,'wb') as f:
+                for chunk in req.iter_content(chunk_size=8192):
+                    if chunk:
+                        f.write(chunk)
+            return filename
+    except Exception as e:
+        print(e)
+        return None
+
+downloadLink = 'https://thumbs.dreamstime.com/z/close-up-inside-okra-flowe-45588243.jpg'
+
+downloadFile(downloadLink,'downloads/testImage.png')
