706ebc47629472b02163bcc66594f9321647c9c2
IsuruLakshan170 <82941889+IsuruLakshan170@users.noreply.github.com>
2023-03-07 16:54:37 +0530

    add

--- encodeParameter.py
+++ encodeParameter.py
@@ -7,35 +7,11 @@ import modelAccuracy as ma
 import dataSetSplit as sp
 import pickle
 
-def load():
-    x_train_np, y_train_np,x_test_np,y_test_np =sp.splitDataset()
-
-    model = mg.create_model()
-
-    print("Local Model ------> ")
-    model.load_weights('modelData/model_weights.h5')
-    ma.getModelAccuracy(model,x_test_np,y_test_np)
-    receivedModelParameters  = model.get_weights()
-    
-    #encode the model
-    model_by= pickle.dumps(receivedModelParameters)
-    model_B64 =base64.b16encode(model_by)
-    #decode the model
-    decode_b64 = base64.b16decode(model_B64)
-    decode_model_weights=pickle.loads(decode_b64)
-    
-    
-    model2 = mg.create_model()
-    model2.set_weights(decode_model_weights)
-    ma.getModelAccuracy(model2,x_test_np,y_test_np)
-
-    
-
 
 def encodeModelParameters(x_test_np,y_test_np):
-    print("Encoding ------> ")
+    print("Encoding ----------------> ")
     model = mg.create_model()
-    model.load_weights('modelData/model_weights.h5')
+    model.load_weights('receivedModelParameter/model_weights_1.h5')
     ma.getModelAccuracy(model,x_test_np,y_test_np)
     receivedModelParameters  = model.get_weights()
     
@@ -45,7 +21,7 @@ def encodeModelParameters(x_test_np,y_test_np):
     return encoded_message
 
 def decodeModelParameters(encoded_message,x_test_np,y_test_np):
-    print("Decoding ------> ")
+    print("Decoding ----------------> ")
     model = mg.create_model()
     #decode the model
     decode_b64 = base64.b16decode(encoded_message)
