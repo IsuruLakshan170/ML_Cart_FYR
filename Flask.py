8bca7e407d508490ff362031cb4fb946ceedf12d
IsuruLakshan170 <82941889+IsuruLakshan170@users.noreply.github.com>
2023-03-08 11:54:53 +0530

    add

--- Flask.py
+++ Flask.py
@@ -4,12 +4,16 @@ import threading
 import Main as im
 import writeFile as wf
 import serverSock
+import QRScanner
+import datetime
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def login():
-    return render_template('index.html')
+    # QRScanner.QRReader()
+   return render_template('index.html')
 
 @app.route("/result", methods =['POST',"GET"])
 def result():
@@ -26,19 +30,28 @@ def start():
     im.initProject()
     return render_template("index.html")
 
+@app.route('/getItems', methods =['POST',"GET"])
+def getItems():
+    # results = QRScanner.QRReader()
+    output = request.form.to_dict()
+    current_time = datetime.datetime.now().time()
+    print("Return successful",current_time)
+    return render_template('index.html',res=current_time)
+
 if __name__ == '__main__':
     # Create two threads: one for running the Flask app, the other for running server connections
     flask_thread = threading.Thread(target=app.run)
     train_thread = threading.Thread(target=serverSock.serverConnect)
 
+    QRScanner_thread = threading.Thread(target=QRScanner.QRReader)
     # Start the threads
     train_thread.start()
     flask_thread.start()
-
+    QRScanner_thread.start()
     # Wait for the threads to finish
     flask_thread.join()
     train_thread.join()
-
+    QRScanner_thread.join()
 
 
 
---
+++ QRScanner.py
@@ -0,0 +1,42 @@
+import cv2
+from pyzbar.pyzbar import decode
+import time
+import writeFile as wf
+import requests
+import Flask
+import threading
+
+def QRReader():
+    cam = cv2.VideoCapture(0)
+    cam.set(5,640)
+    cam.set(6,480)
+
+    camera = True
+    while camera == True:
+        suceess,frame = cam.read()
+        
+        for i in decode(frame):
+            # print(i,type)
+            decodeItem =i.data.decode('utf-8')
+            print(decodeItem)
+            month = 3
+            item = decodeItem
+            gender = 0
+            if decodeItem in "Item 1" :
+                item = 1
+                wf.writetoCSV(month, item, gender)
+                # url = 'http://127.0.0.1:5000/getItems'
+                # data = {'res': 55}
+                # response = requests.post(url, data=data)
+                # print(response.text)
+            elif decodeItem in "Item 2" :
+                item = 2
+                wf.writetoCSV(month, item, gender)
+                # Flask.getItems()
+            time.sleep(6)
+            # return decodeItem
+        
+    # return decodeItem
+        # cv2.imshow("QR Scanner",frame)
+        # cv2.waitKey(3)
+
